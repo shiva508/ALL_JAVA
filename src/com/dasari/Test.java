f8ebdcdcc99d6ce270fbc89a4b81458a00eb8f84
shiva508 <dasarishiva1@gmail.com>
2020-12-19 19:58:29 +0530

    Practice

--- src/com/dasari/Test.java
+++
@@ -1,8 +0,0 @@
-package com.dasari;
-
-public class Test {
-
-	public static void main(String[] args) {
-	}
-
-}
---
+++ src/com/dasari/oops/Parent.java
@@ -0,0 +1,5 @@
+package com.dasari.oops;
+
+public class Parent {
+	public Integer a=20;
+}
