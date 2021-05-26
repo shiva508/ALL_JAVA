cf0f10256cb3fc6b64dbbb5364c6d3a6981609d9
shiva508 <dasarishiva1@gmail.com>
2021-05-26 19:59:57 +0530

    Concat

---
+++ src/com/dasari/string/concatoperation/StringConcatOperation.java
@@ -0,0 +1,10 @@
+package com.dasari.string.concatoperation;
+
+public class StringConcatOperation {
+	public static void main(String[] args) {
+		String cancatData = "Shiva";
+		System.out.println(cancatData.hashCode());
+		cancatData += " Dasari";
+		System.out.println(cancatData.hashCode());
+	}
+}
---
+++ src/com/dasari/thread/callablefuture/CallableFutureSimulatorWithCollection.java
@@ -0,0 +1,22 @@
+package com.dasari.thread.callablefuture;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableFutureSimulatorWithCollection {
+	public static void main(String[] args) {
+		List<Integer> integerList = Arrays.asList(1, 2, 3, 4);
+		  
+	}
+
+}
