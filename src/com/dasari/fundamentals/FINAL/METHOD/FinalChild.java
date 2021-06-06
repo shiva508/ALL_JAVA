4fcf5d9a630165daf26c0fa38a6df1304a2fa903
shiva508 <dasarishiva1@gmail.com>
2021-06-06 21:39:32 +0530

    Final

---
+++ src/com/dasari/fundamentals/FINAL/METHOD/FinalChild.java
@@ -0,0 +1,8 @@
+package com.dasari.fundamentals.FINAL.METHOD;
+
+public class FinalChild extends FinalParent {
+	@Override
+	public void databaseObject() {
+		System.out.println("MySQL");
+	}
+}
---
+++ src/com/dasari/fundamentals/FINAL/METHOD/FinalParent.java
@@ -0,0 +1,11 @@
+package com.dasari.fundamentals.FINAL.METHOD;
+
+public class FinalParent {
+	public void databaseObject() {
+		System.out.println("ORACLE");
+	}
+	public final void connectionFactory() {
+		System.out.println("Connection Factory");
+	}
+	
+}
