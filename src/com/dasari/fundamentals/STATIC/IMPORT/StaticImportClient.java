4618c2d816353219dcb073a5b028dde54b64bbb0
shiva508 <dasarishiva1@gmail.com>
2021-07-17 17:48:15 +0530

    Fundamentals

---
+++ src/com/dasari/fundamentals/STATIC/IMPORT/StaticImportClient.java
@@ -0,0 +1,9 @@
+package com.dasari.fundamentals.STATIC.IMPORT;
+
+import static java.lang.Math.*;
+
+public class StaticImportClient {
+	public static void main(String[] args) {
+		System.out.println(pow(3, 4));
+	}
+}
